4679939b71c1f3813eb0685641e8e653175c53e6
Bhavani <kamadibhavani16@gmail.com>
2025-05-20 14:27:35 +0530

    changes

--- src/Components/webinars-Details/WebinarDetails.jsx
+++ src/Components/webinars-Details/WebinarDetails.jsx
@@ -1,31 +1,61 @@
 // src\Components\webinars-Details\WebinarDetails.jsx
 import React,{useState,useEffect}from 'react'
-import { useLocation } from 'react-router-dom';
+import { useParams,useLocation ,useNavigate} from 'react-router-dom';
 import './webnaiers.css'
 import axios from 'axios';
 import baseurl from '../../baseUrl';
 
 
 function WebinarDetails() {
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+
+  const [webinar, setWebinar] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+  const formData = new FormData();
+  formData.append('action', 'Displays');
+  formData.append('id', id);
+
+  axios.post(`${baseurl}/saveWebinar.php`, formData)
+    .then(response => {
+      if (response.data && response.data.data && response.data.data.length > 0) {
+        
+        const fetchedStory = response.data.data[0];
+        console.log(fetchedStory);
+        fetchedStory.profile_image = `${baseurl}/${fetchedStory.profile_image}`;
+        fetchedStory.main_image = `${baseurl}/${fetchedStory.main_image}`;
+        fetchedStory.image = `${baseurl}/${fetchedStory.image}`;
+        setWebinar(fetchedStory);
+      } else {
+        setWebinar(null);
+      }
+      setLoading(false);
+    })
+    .catch(error => {
+      console.error("There was an error fetching the data!", error);
+      setLoading(false);
+    });
+}, [id]);
         const location = useLocation();
-      const { event } = location.state || {};
-       if (!event) return <p>No Event Data</p>;
+      // const { event } = location.state || {};
+       if (!webinar) return <p>No Event Data</p>;
   return (
      <div className='container py-5 mt-5'>
       <div className='row'>
         <div className='col-lg-12 col-md-12 col-12' id='imageData'>
-           <img src= {event.img} alt='test' />
+           <img src= {webinar.main_image} alt='test' />
            <div className='row'>
             <div className='col-lg-8 col-md-8' id='demo'>
-               <h2>{event.title}</h2>
-               <p>30 April 2025</p>
+               <h2>{webinar.webinar_name}</h2>
+               <p>{webinar.date}</p>
 
-               <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
-               <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
-               <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
-                <img src= {event.img} alt='test' />
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+               <p>{webinar.description1}</p>
+                <img src= {webinar.image} alt='test' />
+                <p>{webinar.description2}</p>
             </div>
              <div className='col-lg-4 col-md-4'>
               {/* emty fill */}
